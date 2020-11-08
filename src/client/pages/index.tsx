3f5a5709b56a6f7c473040580f2ee90b0d85ba9e
chris-bautista <chris.bautista@hulu.com>
2020-11-07 16:50:21 -0800

    chore: adds googletag

--- src/client/pages/index.tsx
+++ src/client/pages/index.tsx
@@ -1,14 +1,8 @@
-import Head from 'next/head';
-
 import styles from '../styles/Home.module.scss';
 
 export default function Home() {
   return (
     <div className={styles.wrapper}>
-      <Head>
-        <title>NarutoQL | Naruto GraphQL</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <div className={styles.hero}>
         <img className={styles.img} src="test.png" />
         <div className={styles.metadata}>
